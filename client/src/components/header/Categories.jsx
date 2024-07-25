b0dc12c443c4585c1bf2e257888618e7e069bb66
sumonDev13 <sumonmondal1530765@gmail.com>
2024-07-26 02:28:53 +0600

    factoring ui

--- client/src/components/header/Categories.jsx
+++ client/src/components/header/Categories.jsx
@@ -1,64 +1,64 @@
-import mobiles from "../../assets/images/Categories/phone.png";
-import fashion from "../../assets/images/Categories/fashion.png";
-import electronics from "../../assets/images/Categories/electronics.png";
-import home from "../../assets/images/Categories/home.png";
-import travel from "../../assets/images/Categories/travel.png";
-import appliances from "../../assets/images/Categories/appliances.png";
-import furniture from "../../assets/images/Categories/furniture.png";
-import beauty from "../../assets/images/Categories/beauty.png";
-import grocery from "../../assets/images/Categories/grocery.png";
+import mailingBags from '../../assets/images/mailbagCard.jpeg';
+import chocolate from '../../assets/images/chocklateCard.jpeg';
+import battery from '../../assets/images/batteryCard.jpeg';
+// import home from "../../assets/images/Categories/home.png";
+// import travel from "../../assets/images/Categories/travel.png";
+// import appliances from "../../assets/images/Categories/appliances.png";
+// import furniture from "../../assets/images/Categories/furniture.png";
+// import beauty from "../../assets/images/Categories/beauty.png";
+// import grocery from "../../assets/images/Categories/grocery.png";
 import { Link } from "react-router-dom";
 
 const catNav = [
     {
-        name: "Mobiles",
-        icon: mobiles,
+        name: "Chocolate",
+        icon: chocolate,
     },
     {
-        name: "Fashion",
-        icon: fashion,
+        name: "Battery",
+        icon: battery,
     },
     {
-        name: "Electronics",
-        icon: electronics,
+        name: "Mailing Bags",
+        icon: mailingBags,
     },
-    {
-        name: "Home",
-        icon: home,
-    },
-    {
-        name: "Travel",
-        icon: travel,
-    },
-    {
-        name: "Appliances",
-        icon: appliances,
-    },
-    {
-        name: "Furniture",
-        icon: furniture,
-    },
-    {
-        name: "Beauty,Toys & more",
-        icon: beauty,
-    },
-    {
-        name: "Grocery",
-        icon: grocery,
-    },
+    // {
+    //     name: "Home",
+    //     icon: home,
+    // },
+    // {
+    //     name: "Travel",
+    //     icon: travel,
+    // },
+    // {
+    //     name: "Appliances",
+    //     icon: appliances,
+    // },
+    // {
+    //     name: "Furniture",
+    //     icon: furniture,
+    // },
+    // {
+    //     name: "Beauty,Toys & more",
+    //     icon: beauty,
+    // },
+    // {
+    //     name: "Grocery",
+    //     icon: grocery,
+    // },
 ];
 
 const Categories = () => {
     return (
-        <section className="hidden sm:block bg-white p-0 min-w-full px-12 shadow overflow-hidden">
-            <div className="flex items-center justify-between group">
+        <section className="sm:block bg-white p-0 min-w-full px-12 shadow overflow-hidden">
+            <div className="flex flex-col sm:flex sm:flex-row items-center justify-between group">
                 {catNav.map((item, i) => (
                     <Link
                         to={`/products?category=${item.name}`}
                         className="flex flex-col gap-1 items-center p-2"
                         key={i}
                     >
-                        <div className="h-16 w-16 ">
+                        <div className="h-40 w-96 m-4">
                             <img
                                 draggable="false"
                                 className="h-full w-full object-contain"
